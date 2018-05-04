426c5ff854de6e839687506db45de957c2ddf766
Erwan Quelin <erwan.quelin@gmail.com>
2018-05-04 10:47:58 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
 # vsanmetrics
-Extract VMware vSAN metrics to InfluxDB 
+
+vsanmetrics is a tool written in Python for collecting usage and performance metrics from a VMware vSAN cluster and translating them in [InfluxDB's line protocol](https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md).
+
+It can be useful to send metrics in a [InfluxDB](https://www.influxdata.com/) database with the help of [Telegraf](https://www.influxdata.com/time-series-platform/telegraf/) and then display metrics in [Grafana](https://grafana.com/).
+
+## Prerequisites
+
+- Python (This script has been tested with python 2.7.12)
+- [Pyvmomi](https://github.com/vmware/pyvmomi#installing)
+
+> You can install pyvmomi with pip -> `pip install pyvmomi`
+
+- [VMware vSAN Management SDK for python](https://code.vmware.com/web/sdk/6.7.0/vsan-python)
+
+> To use the vSAN Python bindings, download the SDK and place `vsanmgmtObjects.py` and `vsanapiutis.py` on a path where your Python applications can import library or in the same folder than `vsanmetrics.py`.
+
+## Installation
+
+- Download the script `vsanmetrics.py`
+- On linux box, make the script executable
+
+```bash
+% chmod +x ./vsanmetrics
+```
+
+- Run the script with the -h parameter to check if it works
+
+```bash
+% ./vsanmetrics -h
+
+usage: vsanmetrics.py [-h] -s VCENTER [-o PORT] -u USER [-p PASSWORD] -c CLUSTERNAME
+
+Get vSAN cluster statistics
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s VCENTER, --vcenter VCENTER
+                        Remote vcenter to connect to
+  -o PORT, --port PORT  Port to connect on
+  -u USER, --user USER  User name to use when connecting to vcenter
+  -p PASSWORD, --password PASSWORD
+                        Password to use when connecting to vcenter
+  -c CLUSTERNAME, --cluster_name CLUSTERNAME
+                        Cluster Name
+```
+
+- Run the script against a vSAN cluster. It should output some metrics like in this example
+
+```bash
+./vsanmetrics.py -s vcenter.example.com -u administrator@vsphere.local -p MyAwesomePassword -c VSAN-CLUSTER
+
+capacity_global,scope=global,vcenter=vcenter.example.com,cluster=VSAN-CLUSTER totalCapacityB=7200999211008,freeCapacityB=1683354550260 1525422314084382976
+capacity_summary,scope=summary,vcenter=vcenter.example.com,cluster=VSAN-CLUSTER temporaryOverheadB=0,physicalUsedB=2636212338688,primaryCapacityB=2688980877312,usedB=5380734189568,reservedCapacityB=3607749040540,overReservedB=2744521850880,provisionCapacityB=6986210377728,overheadB=2828663783436 1525422314084382976
+capacity_vmswap,scope=vmswap,vcenter=vcenter.example.com,cluster=VSAN-CLUSTER temporaryOverheadB=0,physicalUsedB=8422162432,primaryCapacityB=177330978816,usedB=355240771584,reservedCapacityB=355089776640,overReservedB=346818609152,overheadB=177909792768 1525422314084382976
+capacity_checksumOverhead,scope=checksumOverhead,vcenter=vcenter.example.com,cluster=VSAN-CLUSTER temporaryOverheadB=0,physicalUsedB=0,primaryCapacityB=0,usedB=8858370048,reservedCapacityB=0,overReservedB=0,overheadB=8858370048 1525422314084382976
+```
+
+## Using vsanmetrics with Telegraf
+
+The `exec` input plugin of Telegraf executes the `commands` on every interval and parses metrics from their output in any one of the accepted [Input Data Formats](https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md).
+
+> Don't forget to configure Telegraf to output data to InfluxDB !
+
+`vsanmetrics` output the metrics in InfluxDB's line protocol. Telegraf will parse them and send them to the InfluxDB database.
+
+`vsanmetrics` and `pyvmomi` should be available by the user who run the Telegraf service. (typically root on Linux boxes...).
+
+> TIP: On Linux, install pyvmomi with the command `sudo -H pip install pyvmomi` to make it available to the root user. 
+
+Here is an example of a working telegraf's config file:
+
+```Toml
+###############################################################################
+#                            INPUT PLUGINS                                    #
+###############################################################################
+
+[[inputs.exec]]
+  # Shell/commands array
+  # Full command line to executable with parameters, or a glob pattern to run all matching files.
+  commands = ["/path/to/script/vsanmetrics.py -s vcenter01.example.com -u administrator@vsphere.local -p MyAwesomePassword -c VSAN-CLUSTER"]
+
+  # Timeout for each command to complete.
+  timeout = "60s"
+
+  # Data format to consume.
+  # NOTE json only reads numerical measurements, strings and booleans are ignored.
+  data_format = "influx"
+
+  interval = "300s"
+```
+
+If needed, you can specify more than one input plugin. It might be useful if you want to gather different statistics with different intervals or if you want to query different vSAN clusters.
+
+```Toml
+###############################################################################
+#                            INPUT PLUGINS                                    #
+###############################################################################
+
+[[inputs.exec]]
+  # Shell/commands array
+  # Full command line to executable with parameters, or a glob pattern to run all matching files.
+  commands = ["/path/to/script/vsanmetrics.py -s vcenter01.example.com -u administrator@vsphere.local -p MyAwesomePassword -c VSAN-CLUSTER"]
+
+  # Timeout for each command to complete.
+  timeout = "60s"
+
+  # Data format to consume.
+  # NOTE json only reads numerical measurements, strings and booleans are ignored.
+  data_format = "influx"
+
+  interval = "300s"
+
+[[inputs.exec]]
+  # Shell/commands array
+  # Full command line to executable with parameters, or a glob pattern to run all matching files.
+  commands = ["/path/to/script/vsanmetrics.py -s vcenter02.example.com -u administrator@vsphere.local -p MyAwesomePassword -c VSAN-CLUSTER"]
+
+  # Timeout for each command to complete.
+  timeout = "60s"
+
+  # Data format to consume.
+  # NOTE json only reads numerical measurements, strings and booleans are ignored.
+  data_format = "influx"
+
+  interval = "300s"
+
+# Author
+**Erwan Quélin**
+- <https://github.com/equelin>
+- <https://twitter.com/erwanquelin>
+
+# License
+
+Copyright 2018 Erwan Quelin and the community.
+
+Licensed under the Apache License 2.0.
---
+++ vsanmetrics.py
@@ -0,0 +1,446 @@
+#!/usr/bin/env python
+
+# Erwan Quelin - erwan.quelin@gmail.com
+
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import pbm, VmomiSupport, SoapStubAdapter, vim, vmodl
+
+import argparse
+import atexit
+import getpass
+from datetime import datetime, timedelta
+import time
+import ssl
+
+import vsanapiutils
+import vsanmgmtObjects
+
+
+def get_args():  
+    parser = argparse.ArgumentParser(
+        description='Get vSAN cluster statistics')
+
+    parser.add_argument('-s', '--vcenter',
+                        required=True,
+                        action='store',
+                        help='Remote vcenter to connect to')
+
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use when connecting to vcenter')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use when connecting to vcenter')
+
+    parser.add_argument('-c', '--cluster_name', 
+                        dest='clusterName', 
+                        required=True,
+                        help='Cluster Name')
+
+    args = parser.parse_args()
+    if not args.password:
+        args. password = getpass.getpass(
+            prompt='Enter password for host %s and user %s: ' %
+                   (args.vcenter, args.user))
+
+    return args
+
+# Get cluster informations
+def getClusterInstance(clusterName, content):
+   searchIndex = content.searchIndex
+   datacenters = content.rootFolder.childEntity
+   for datacenter in datacenters:
+      cluster = searchIndex.FindChild(datacenter.hostFolder, clusterName)
+      if cluster is not None:
+         return cluster
+   return None
+
+# Get hosts informations (hostname and disks)
+def getHostsInfos(hosts):
+    disksinfos = {}
+    hostinfos = {}
+
+    for membershipList in hosts[0].runtime.vsanRuntimeInfo.membershipList:
+        hostinfos[membershipList.nodeUuid] = membershipList.hostname
+
+    for host in hosts:
+
+        diskAll = host.configManager.vsanSystem.QueryDisksForVsan()
+
+        for disk in diskAll:
+            if disk.state == 'inUse':
+                disksinfos[disk.vsanUuid] = disk.disk.canonicalName
+
+    return disksinfos,hostinfos
+
+# Get all VM managed by vCenter, return array with name and uuid of the VMs
+def getVMs(content):
+    container = content.rootFolder  # starting point to look into
+    viewType = [vim.VirtualMachine]  # object types to look for
+    recursive = True  # whether we should look into it recursively
+    containerView = content.viewManager.CreateContainerView(container, viewType, recursive)
+
+    children = containerView.view
+
+    vms = {}
+
+    for child in children:
+        vms[child.summary.config.instanceUuid] = child.summary.config.name
+
+    return vms
+
+# OUtput data in the Influx Line protocol format
+def printInfluxLineProtocol(measurement,tags,fields,timestamp):
+    result = "%s,%s %s %i" % (measurement,arrayToString(tags),arrayToString(fields),timestamp)
+    print(result)
+
+# Convert time in string format to epoch timestamp (nanosecond)
+def convertStrToTimestamp(str):
+    sec = time.mktime(datetime.strptime(str, "%Y-%m-%d %H:%M:%S").timetuple())
+
+    ns = int(sec * 1000000000)
+
+    return ns
+
+# parse EntytyRefID, convert to tags
+def parseEntityRefId(measurement,entityRefId,hosts,vms,disks):
+
+    tags = {}
+
+    if measurement == 'vscsi':
+        entityRefId = entityRefId.split("|")
+        split = entityRefId[0].split(":")
+
+        tags['uuid'] = split[1]
+        tags['vscsi'] = entityRefId[1]
+        tags['vmname'] = vms[split[1]]
+    else:
+        entityRefId = entityRefId.split(":")
+
+        if measurement == 'cluster-domclient':
+            tags['uuid'] = entityRefId[1]
+
+        if measurement == 'cluster-domcompmgr':
+            tags['uuid'] = entityRefId[1]
+
+        if measurement == 'host-domclient':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+        if measurement == 'host-domcompmgr':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+        if measurement == 'cache-disk':
+            tags['uuid'] = entityRefId[1]
+            tags['naa'] = disks[entityRefId[1]]
+
+        if measurement == 'capacity-disk':
+            tags['uuid'] = entityRefId[1]
+            tags['naa'] = disks[entityRefId[1]]
+
+        if measurement == 'disk-group':
+            tags['uuid'] = entityRefId[1]
+
+        if measurement == 'virtual-machine':
+            tags['uuid'] = entityRefId[1]
+            tags['vmname'] = vms[entityRefId[1]]
+
+        if measurement == 'virtual-disk':
+            split = entityRefId[1].split("/")     
+
+            tags['uuid'] = split[0]
+            tags['disk'] = split[1]
+
+        if measurement == 'vsan-vnic-net':
+            split = entityRefId[1].split("|")     
+
+            tags['uuid'] = split[0]
+            tags['hostname'] = hosts[split[0]] 
+            tags['stack'] = split[1]
+            tags['vmk'] = split[2]
+
+        if measurement == 'vsan-host-net':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+        if measurement == 'vsan-pnic-net':
+
+            split = entityRefId[1].split("|")     
+
+            tags['uuid'] = split[0]
+            tags['hostname'] = hosts[split[0]] 
+            tags['vmnic'] = split[1]
+
+        if measurement == 'vsan-iscsi-host':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+        if measurement == 'vsan-iscsi-target':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+        if measurement == 'vsan-iscsi-lun':
+            tags['uuid'] = entityRefId[1]
+            tags['hostname'] = hosts[entityRefId[1]] 
+
+    return tags
+
+# Convert array to a string compatible with influxdb line protocol tags or fields
+def arrayToString(data):
+    i = 0
+    result = ""
+
+    for key,val in data.items():
+        if i == 0:
+            result = "%s=%s" % (key,val)
+        else:
+            result = result + ",%s=%s" % (key,val)
+        i = i + 1 
+    return result
+
+def parseVsanObjectSpaceSummary(data):
+    fields = {}
+
+    fields['overheadB'] = data.overheadB
+    fields['overReservedB'] = data.overReservedB
+    fields['physicalUsedB'] = data.physicalUsedB
+    fields['primaryCapacityB'] = data.primaryCapacityB
+    fields['reservedCapacityB'] = data.reservedCapacityB
+    fields['temporaryOverheadB'] = data.temporaryOverheadB
+    fields['usedB'] = data.usedB
+    
+    if data.provisionCapacityB:
+        fields['provisionCapacityB'] = data.provisionCapacityB
+    
+    return fields
+
+def parseVimVsanDataEfficiencyCapacityState(data):
+    fields = {}
+
+    fields['dedupMetadataSize'] = data.dedupMetadataSize
+    fields['logicalCapacity'] = data.logicalCapacity
+    fields['logicalCapacityUsed'] = data.logicalCapacityUsed
+    fields['physicalCapacity'] = data.physicalCapacity
+    fields['physicalCapacityUsed'] = data.physicalCapacityUsed
+
+    return fields    
+
+def parseCapacity(scope,data,tagsbase,timestamp):
+
+    tags = {}
+    fields = {}
+
+    tags['scope'] = scope
+    tags.update(tagsbase)
+    measurement = 'capacity_' + scope
+
+    if scope == 'global':
+        fields['freeCapacityB'] = data.freeCapacityB
+        fields['totalCapacityB'] = data.totalCapacityB
+
+    elif scope == 'summary':
+        fields = parseVsanObjectSpaceSummary(data.spaceOverview)
+
+    elif scope == 'efficientcapacity':
+        fields = parseVimVsanDataEfficiencyCapacityState(data.efficientCapacity)
+    else:
+        fields = parseVsanObjectSpaceSummary(data)
+
+    printInfluxLineProtocol(measurement,tags,fields,timestamp)
+
+# Main...
+def main():
+
+    # Don't check for valid certificate
+    context = ssl._create_unverified_context()
+
+    # Parse CLI arguments
+    args = get_args()
+
+    # Connect to vCenter
+    try:
+        si = SmartConnect(host=args.vcenter,
+                                                user=args.user,
+                                                pwd=args.password,
+                                                port=int(args.port),
+                                                sslContext=context)
+        if not si:
+            print("Could not connect to the specified host using specified "
+                  "username and password")
+
+            return -1
+    except vmodl.MethodFault as e:
+        print("Caught vmodl fault : " + e.msg)
+        return -1
+
+    except Exception as e:
+        print("Caught exception : " + str(e))
+        return -1
+
+    # Disconnect to vcenter at the end
+    atexit.register(Disconnect, si)
+
+    # Get content informations
+    content = si.RetrieveContent()
+
+    # Get Info about cluster
+    cluster_obj = getClusterInstance(args.clusterName,content)
+    
+    # Exit if the cluster provided in the arguments is not available
+    if not cluster_obj:
+        print 'The required cluster not found in inventory, validate input. Aborting test'
+        exit()
+
+    # Initiate tags with vcenter and cluster name
+    tagsbase = {}
+    tagsbase['vcenter'] = args.vcenter
+    tagsbase['cluster'] = args.clusterName
+
+    # Get VM uuid/names
+    vms = getVMs(content)
+
+
+    #for host in cluster_obj.host:
+     #   host.QueryHostStatus()
+
+
+    # Get disks uuid/names et hosts uuid/names
+    diskinfos, hostinfos = getHostsInfos(cluster_obj.host)
+
+    apiVersion = vsanapiutils.GetLatestVmodlVersion(args.vcenter)
+    vcMos = vsanapiutils.GetVsanVcMos(si._stub, context=context, version=apiVersion)
+
+    ## CAPACITY
+    vsanSpaceReportSystem = vcMos['vsan-cluster-space-report-system']
+
+    try:
+        spaceReport = vsanSpaceReportSystem.VsanQuerySpaceUsage(
+            cluster = cluster_obj
+            )
+    except vmodl.fault.InvalidArgument as e:
+        print("Caught InvalidArgument exception : " + str(e))
+        return -1    
+    except vmodl.fault.NotSupported as e:
+        print("Caught NotSupported exception : " + str(e))
+        return -1
+
+    except vmodl.fault.RuntimeFault as e:
+        print("Caught RuntimeFault exception : " + str(e))
+        return -1
+
+    timestamp = int(time.time() * 1000000000)
+
+    parseCapacity('global',spaceReport,tagsbase,timestamp)
+    parseCapacity('summary',spaceReport,tagsbase,timestamp)
+
+    if spaceReport.efficientCapacity:
+        parseCapacity('efficientcapacity',spaceReport,tagsbase,timestamp)
+
+    for object in spaceReport.spaceDetail.spaceUsageByObjectType:
+        parseCapacity(object.objType,object,tagsbase,timestamp)
+
+    ## PERFORMANCE
+    vsanPerfSystem =  vcMos['vsan-performance-manager']
+
+    # Gather a list of the available entity types (ex: vsan-host-net)
+    entityTypes = vsanPerfSystem.VsanPerfGetSupportedEntityTypes()
+
+    # query interval, last 10 minutes 
+    endTime = datetime.utcnow()
+    startTime = endTime + timedelta(minutes=-10)
+
+    for entities in entityTypes:
+
+        entitieName = entities.name
+
+        labels = []
+
+        # Gather all labels related to the entity (ex: iopsread, iopswrite...)
+        for entity in entities.graphs:
+
+            for metric in entity.metrics:
+
+                labels.append(metric.label)
+
+        # Build entity 
+        entity = '%s:*' % (entities.name)
+
+        # Build spec object
+        spec = vim.cluster.VsanPerfQuerySpec(
+            endTime = endTime,
+            entityRefId = entity,
+            labels = labels,
+            startTime = startTime
+        )
+
+        # Get statistics
+        try:
+            metrics = vsanPerfSystem.VsanPerfQueryPerf(
+                querySpecs = [spec],
+                cluster = cluster_obj
+            )
+
+        except vmodl.fault.InvalidArgument as e:
+            print("Caught InvalidArgument exception : " + str(e))
+            return -1
+
+        except vmodl.fault.NotFound as e:
+            print("Caught NotFound exception : " + str(e))
+            return -1
+
+        except vmodl.fault.NotSupported as e:
+            print("Caught NotSupported exception : " + str(e))
+            return -1
+
+        except vmodl.fault.RuntimeFault as e:
+            print("Caught RuntimeFault exception : " + str(e))
+            return -1
+
+        except vmodl.fault.Timedout as e:
+            print("Caught Timedout exception : " + str(e))
+            return -1
+
+        except vmodl.fault.VsanNodeNotMaster as e:
+            print("Caught VsanNodeNotMaster exception : " + str(e))
+            return -1
+
+        for metric in metrics:
+
+            if not metric.sampleInfo == "":
+
+                measurement = entitieName    
+
+                sampleInfos = metric.sampleInfo.split(",")
+                lenValues = len(sampleInfos)
+
+                timestamp = convertStrToTimestamp(sampleInfos[lenValues - 1])
+
+                tags = parseEntityRefId(measurement,metric.entityRefId,hostinfos,vms,diskinfos)
+
+                tags.update(tagsbase)
+
+                fields = {}
+
+                for value in metric.value:
+
+                    listValue = value.values.split(",")
+
+                    fields[value.metricId.label] = float(listValue[lenValues - 1])
+
+                printInfluxLineProtocol(measurement,tags,fields,timestamp) 
+
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
