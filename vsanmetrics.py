777aad73699f6ad8956f685b38b2f7d6be76556f
bdeam <bradleydeam@gmail.com>
2019-06-07 08:35:11 +0930

    Added escape characters for VM's with whitespace in their name

--- vsanmetrics.py
+++ vsanmetrics.py
@@ -225,8 +225,10 @@ def getVMs(cluster):
 
     for host in cluster.host:  # Iterate through Hosts in the Cluster
         for vm in host.vm:  # Iterate through each VM on the host
-            vms[vm.summary.config.instanceUuid] = vm.summary.config.name
-
+            vmname = vm.summary.config.name
+            # Check for white space in VM's name, and replace with escape characters
+            vmname = "\ ".join(vmname.split())
+            vms[vm.summary.config.instanceUuid] = vmname
     return vms
 
 
